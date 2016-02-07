d9d04aebd4a2426b7b86ea992650b375f0b940a6
Matt McCutchen <rmccutch@mit.edu>
2016-02-07 02:29:48 -0500

    convert.sh: Add --debug option.

--- convert.sh
+++ convert.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e -x
 
+if [ "$1" == "--debug" ]; then
+	maybe_debug=node-debug
+else
+	maybe_debug=
+fi
+
 # I've forgotten this enough times.  Saves a step for new checkouts too.
 make -C tests/coffeescript-to-typescript
 
 ./convert-cleanup.sh
 
 old=($(find src -name '*.coffee'))
-tests/coffeescript-to-typescript/bin/coffee -cma ${old[@]} &>coffee-to-ts-errors.txt
+$maybe_debug tests/coffeescript-to-typescript/bin/coffee -cma ${old[@]} &>coffee-to-ts-errors.txt
 #find src -name '*.js' -delete  # Why are these generated??  2016-01-29: I assume it's the typescript-property-accumulator pass, now disabled.
 
 # These apply only to the final conversion.
