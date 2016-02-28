e7edbd18e6ef8ea9d37936e8a0566351aeac5ec7
Matt McCutchen <rmccutch@mit.edu>
2016-02-28 17:14:13 -0500

    Add the remaining library type definitions, and stub out some libraries for which usable type definitions are not available.
    179 TypeScript errors.

---
+++ typings/extra.d.ts
@@ -0,0 +1,10 @@
+// Stubs for now.
+
+// There's a partial type definition that doesn't include the Handsontable
+// class.  Declare a stub, and then see what we can wire up and what we have to
+// add.
+declare var Handsontable;
+
+// I didn't find a type definition for Jison at all.  We could start one.
+// ~ Matt 2016-02-28
+declare var Jison;
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="meteor/meteor.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="jquery-handsontable/jquery-handsontable.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
